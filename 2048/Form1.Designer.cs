b0f24877e0da83c8259a0639244d5a1a4f2cea5a
taozixiongtai <34221849+taozixiongtai@users.noreply.github.com>
2019-07-02 18:52:32 +0800

    添加项目文件。

---
+++ 2048/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace _2048
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox17 = new System.Windows.Forms.PictureBox();
+            this.pictureBox16 = new System.Windows.Forms.PictureBox();
+            this.pictureBox15 = new System.Windows.Forms.PictureBox();
+            this.pictureBox14 = new System.Windows.Forms.PictureBox();
+            this.pictureBox13 = new System.Windows.Forms.PictureBox();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox17)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox16)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox15)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox17);
+            this.panel1.Controls.Add(this.pictureBox16);
+            this.panel1.Controls.Add(this.pictureBox15);
+            this.panel1.Controls.Add(this.pictureBox14);
+            this.panel1.Controls.Add(this.pictureBox13);
+            this.panel1.Controls.Add(this.pictureBox12);
+            this.panel1.Controls.Add(this.pictureBox11);
+            this.panel1.Controls.Add(this.pictureBox10);
+            this.panel1.Controls.Add(this.pictureBox9);
+            this.panel1.Controls.Add(this.pictureBox7);
+            this.panel1.Controls.Add(this.pictureBox6);
+            this.panel1.Controls.Add(this.pictureBox5);
+            this.panel1.Controls.Add(this.pictureBox4);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(4, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(423, 422);
+            this.panel1.TabIndex = 9;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // pictureBox17
+            // 
+            this.pictureBox17.Location = new System.Drawing.Point(300, 100);
+            this.pictureBox17.Name = "pictureBox17";
+            this.pictureBox17.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox17.TabIndex = 16;
+            this.pictureBox17.TabStop = false;
+            // 
+            // pictureBox16
+            // 
+            this.pictureBox16.Location = new System.Drawing.Point(0, 100);
+            this.pictureBox16.Name = "pictureBox16";
+            this.pictureBox16.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox16.TabIndex = 15;
+            this.pictureBox16.TabStop = false;
+            // 
+            // pictureBox15
+            // 
+            this.pictureBox15.Location = new System.Drawing.Point(0, 200);
+            this.pictureBox15.Name = "pictureBox15";
+            this.pictureBox15.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox15.TabIndex = 14;
+            this.pictureBox15.TabStop = false;
+            // 
+            // pictureBox14
+            // 
+            this.pictureBox14.Location = new System.Drawing.Point(0, 300);
+            this.pictureBox14.Name = "pictureBox14";
+            this.pictureBox14.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox14.TabIndex = 13;
+            this.pictureBox14.TabStop = false;
+            // 
+            // pictureBox13
+            // 
+            this.pictureBox13.Location = new System.Drawing.Point(100, 300);
+            this.pictureBox13.Name = "pictureBox13";
+            this.pictureBox13.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox13.TabIndex = 12;
+            this.pictureBox13.TabStop = false;
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.Location = new System.Drawing.Point(100, 200);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox12.TabIndex = 11;
+            this.pictureBox12.TabStop = false;
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.Location = new System.Drawing.Point(300, 0);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox11.TabIndex = 10;
+            this.pictureBox11.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Location = new System.Drawing.Point(100, 100);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox10.TabIndex = 9;
+            this.pictureBox10.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Location = new System.Drawing.Point(100, 0);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox9.TabIndex = 8;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Location = new System.Drawing.Point(200, 300);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(200, 200);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox6.TabIndex = 5;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(200, 100);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(200, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(300, 300);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(300, 200);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(538, 302);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 12);
+            this.label2.TabIndex = 11;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(448, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(434, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "游戏规则：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(434, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 12);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "方向键：上下左右控";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(436, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 12);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "制方块的移动。";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 464);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox17)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox16)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox15)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox14)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox17;
+        private System.Windows.Forms.PictureBox pictureBox16;
+        private System.Windows.Forms.PictureBox pictureBox15;
+        private System.Windows.Forms.PictureBox pictureBox14;
+        private System.Windows.Forms.PictureBox pictureBox13;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ 2048/Form1.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace _2048
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static PictureBox[,] pbArray = new PictureBox[4, 4];
+
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Image bg = Resource.bg1;
+
+            foreach (PictureBox pb in panel1.Controls)
+            {
+                pb.Image = bg;
+                pb.SizeMode = PictureBoxSizeMode.StretchImage;
+                
+                pbArray[pb.Location.X / 100, pb.Location.Y / 100] = pb;
+
+            }
+
+
+            
+            
+        
+        }
+
+
+        public bool achieve ;
+        public void pipei(PictureBox p1,PictureBox p2)          
+        {
+
+            Label lab1 = p1.Controls.OfType<Label>().ElementAt<Label>(0);
+            Label lab2 = p2.Controls.OfType<Label>().ElementAt<Label>(0);
+
+
+            int i =Convert.ToInt32(lab1.Text);
+            int j =Convert.ToInt32(lab2.Text);
+            if (i==j)
+            {
+                j += i;
+                p1.Controls.Remove(lab1);
+                lab2.Text = j.ToString();
+                achieve = true;
+
+            }
+           
+          
+           
+           
+        
+        
+        }
+
+
+        public void shengcheng()
+        {
+
+            Random local = new Random();
+            int x = local.Next(0, 4);
+            int y = local.Next(0, 4);
+
+            int text = local.Next() % 3;
+
+            while (pbArray[x, y].Controls.OfType<Label>().Any())
+            {
+                x = local.Next(0, 4);
+                y = local.Next(0, 4);
+            }
+
+
+            string a = x.ToString() + y.ToString();
+            label2.Text = label2.Text + a;
+
+
+
+            Label l = new Label();
+            l.BackColor = Color.Red;
+            l.Size = new System.Drawing.Size(100, 100);
+            l.TextAlign = ContentAlignment.MiddleCenter;
+            if (text<=1)
+            {
+                l.Text = "2";
+            }
+            else
+            {
+                l.Text = "4";
+            }
+            
+            l.Font = new System.Drawing.Font("宋体", 25, FontStyle.Bold);
+            l.Parent = pbArray[x, y];
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+       
+
+        public void move(PictureBox pb, int dirction, int x,int y) 
+        {
+
+
+            Label lab = pb.Controls.OfType<Label>().ElementAt<Label>(0);
+            switch (dirction)
+            {
+
+                case 1:
+
+
+
+                    lab.Parent = pbArray[x-1, y ];    
+                    
+                    while((x-1)!=0&&!pbArray[x-2,y].Controls.OfType<Label>().Any())
+                    {
+                        x = x - 1;
+                        lab.Parent = pbArray[x - 1, y];
+                    }
+                    if ((x - 2) >= 0)
+                    {
+                        pipei(pbArray[x-1, y], pbArray[x-2, y]);
+                    }
+                  
+                   
+                    break;
+                case 2:
+                    lab.Parent = pbArray[x+1, y ];
+
+                    while ((x + 1) != 3 && !pbArray[x + 2, y].Controls.OfType<Label>().Any())
+                    {
+                        x = x + 1;
+                        lab.Parent = pbArray[x + 1, y];
+                    }
+                    if ((x + 2) <= 3)
+                    {
+                        pipei(pbArray[x + 1, y], pbArray[x + 2, y]);
+                    }
+                    break;
+                case 3:
+
+                    lab.Parent = pbArray[x, y - 1];                  
+                    while ((y - 1) != 0 && !pbArray[x , y-2].Controls.OfType<Label>().Any())
+                    {
+                        y = y - 1;
+                        lab.Parent = pbArray[x , y-1];
+                    } 
+                  
+                   
+                    if ((y-2)>=0) 
+                    {
+                        pipei(pbArray[x, y-1], pbArray[x, y - 2]);
+                    }
+                    
+                    break;
+                case 4:
+                    lab.Parent = pbArray[x, y +1];
+
+                    while ((y + 1) != 3 && !pbArray[x, y + 2].Controls.OfType<Label>().Any())
+                    {
+                        y = y + 1;
+                        lab.Parent = pbArray[x, y + 1];
+                    }
+
+                    if ((y + 2) <= 3)
+                    {
+                        pipei(pbArray[x, y +1], pbArray[x, y + 2]);
+                    }
+                    break;
+           
+            }
+                                   
+
+
+        }
+
+
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+
+            achieve = false;
+
+            switch (e.KeyCode)
+            {
+                
+
+                case Keys.Up:
+
+                    for (int x = 0; x < 4; x++)
+                    {
+                        for (int y= 1; y < 4; y++)
+                        {
+                           
+
+                            if (pbArray[x, y].Controls.OfType<Label>().Any()&&!pbArray[x, y - 1].Controls.OfType<Label>().Any())
+                                
+                            {
+
+                                move(pbArray[x, y], 3, x, y);
+                                achieve = true;
+                              
+                                
+                            }
+                            if (pbArray[x, y - 1].Controls.OfType<Label>().Any() && pbArray[x, y ].Controls.OfType<Label>().Any())//上面有,而且自己也要有
+                              
+                           {
+                               pipei(pbArray[x, y], pbArray[x, y - 1]);
+                             
+                              
+                           }
+
+                        }
+
+
+                    }
+
+
+                    if (achieve)
+                    {
+                        shengcheng();
+                    }
+                    
+                    break;
+                case Keys.Down:
+                    for (int x = 0; x < 4; x++) 
+                    {
+                        for (int y = 2; y >-1; y--)
+                        {
+                            if (pbArray[x, y].Controls.OfType<Label>().Any() &&! pbArray[x, y +1].Controls.OfType<Label>().Any())
+                            
+                            {
+
+                                move(pbArray[x, y], 4, x, y);
+                                achieve = true;
+                               
+                                
+                            }
+                            if (pbArray[x, y + 1].Controls.OfType<Label>().Any()&&pbArray[x, y ].Controls.OfType<Label>().Any())//下面有
+                            {
+                                pipei(pbArray[x, y], pbArray[x, y + 1]);
+                              
+                                
+                            }
+
+
+                        }
+
+
+                    }
+
+                    if (achieve)
+                    {
+                        shengcheng();
+                    }
+                    
+                    break;
+                case Keys.Right:
+                    for (int x = 2; x >-1; x--) 
+                    {
+                        for (int y = 0; y <4; y++) 
+                        {
+                            if (pbArray[x, y].Controls.OfType<Label>().Any() && !pbArray[x+1, y ].Controls.OfType<Label>().Any())
+                            
+                            {
+
+                                move(pbArray[x, y], 2, x, y);
+                                achieve = true;
+                                
+
+                                
+
+                            }
+                            if (pbArray[x + 1, y].Controls.OfType<Label>().Any() && pbArray[x, y].Controls.OfType<Label>().Any())//右面有
+                            {
+                                pipei(pbArray[x, y], pbArray[x + 1, y]);
+                               
+                               
+                            }
+
+                        }
+
+
+                    }
+
+                    if (achieve)
+                    {
+                        shengcheng();
+                    }
+                    
+                    break;
+                case Keys.Left:
+                    
+                    for (int x = 1; x <4; x++)  
+                    {
+                        for (int y = 0; y < 4; y++)
+                        {
+                            if (pbArray[x, y].Controls.OfType<Label>().Any() && !pbArray[x-1, y ].Controls.OfType<Label>().Any())
+                       
+                            {
+
+                                move(pbArray[x, y], 1, x, y);
+                                achieve = true;
+                               
+                                
+                            }
+                            if (pbArray[x - 1, y].Controls.OfType<Label>().Any() && pbArray[x, y].Controls.OfType<Label>().Any())//左面有
+                            {
+                                pipei(pbArray[x, y], pbArray[x - 1, y]);
+                               
+            
+                            }
+
+                        }
+
+
+                    }
+
+                    if (achieve)
+                    {
+                        shengcheng();
+                    }
+                    
+                    
+                    break;
+                default: break;
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            shengcheng();
+            shengcheng();
+            this.button1.Enabled = false;
+        }
+    }
+}
+
